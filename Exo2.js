61ebf49a3b3b5aa834ba139d1f1d21a3051b60f3
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-27 04:55:39 +0100

    Question 1 terminer:Écrivez un code qui peut donner des notes aux étudiants en fonction de leurs résultats : 80-100, Un 70-89, B 60-69, C 50-59, D 0-49, F

---
+++ Exo2.js
@@ -0,0 +1,13 @@
+// Réponse numero 1
+let numero = prompt('Enter your note');
+if (numero >= 80) {
+    console.log("A");
+} else if (numero >= 70) {
+    console.log("B");
+} else if (numero >= 60) {
+    console.log("C");
+} else if (numero >= 50) {
+    console.log("D");
+} else {
+    console.log("F");
+};

2414bec20d0b97e6d1a6ae4d3f0ce9cead57cca5
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-27 02:06:39 +0100

    Question 1 terminer:Obtenez l'avis de l'utilisateur à l'aide de l'invite (« Entrez votre âge : »). Si l'utilisateur a 18 ans ou plus, donnez un commentaire : « Vous avez l'âge requis pour conduire », mais s'il n'a pas 18 ans, donnez un autre commentaire indiquant d'attendre le nombre d'années dont il a besoin pour atteindre ses 18 ans.

---
+++ Exo1.js
@@ -0,0 +1,7 @@
+// Reponse numero 1
+let premier = prompt("Enter your age:");
+if (premier >= 18) {
+    alert("You are old enough to drive.")
+} else {
+    alert("You are left years to drive.")
+}

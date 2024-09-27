e67109ef486962c2cbf1558a1c4fce9e9079e914
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-27 05:31:03 +0100

    Question 2 terminer:Vérifiez si la saison est l'automne, l'hiver, le printemps ou l'été. Si la saisie de l'utilisateur est : Septembre, octobre ou novembre, la saison est l'automne. Décembre, janvier ou février, la saison est l'hiver. Mars, avril ou mai, la saison est le printemps Juin, juillet ou août, la saison est l'été

--- Exo2.js
+++ Exo2.js
@@ -11,3 +11,23 @@ if (numero >= 80) {
 } else {
     console.log("F");
 };
+
+
+// Réponse numero 2
+let saison = prompt("Entrer le mois").trim();
+const moisAutomne = ["septembre", "octobre", "novembre"];
+const moisHiver = ["décembre", "janvier", "février"];
+const moisPrintemps = ["mars", "avril", "mai"];
+const moisEte = ["juin", "juillet", "août"];
+
+if (moisAutomne.includes(saison)) {
+    console.log("La saison est l'automne");
+} else if (moisHiver.includes(saison)) {
+    console.log("La saison est l'hiver");
+} else if (moisPrintemps.includes(saison)) {
+    console.log("La saison est le printemps");
+} else if (moisEte.includes(saison)) {
+    console.log("La saison est l'été");
+} else {
+    console.log("La valeur mis n'est pas un moi");
+}

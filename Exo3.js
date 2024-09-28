481dc79fc8b01b287bffa10ad780fed22e465c02
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-28 19:47:44 +0100

    Question modifier

--- Exo3.js
+++ Exo3.js
@@ -2,12 +2,46 @@
 // Demander à l'utilisateur de saisir l'année et le mois
 const annee = prompt("Entrez une année (par exemple, 2024) : ");
 const mois = prompt("Entrez un mois (0 pour janvier, 11 pour décembre) : ");
-// Créer une date au premier jour du mois suivant
-const premierDuMoisSuivant = new Date(annee, mois + 1);
-premierDuMoisSuivant.setDate(premierDuMoisSuivant.getDate() - 1);
-
-// Obtenir le nombre de jours du mois
-const nombreDeJours = premierDuMoisSuivant.getDate();
-
+let trans = parseInt(annee);
+const tableauMois = [{"janvier" : 31},{"fevrier" : 28},{"mars" : 30},{"avril" : 30},{"mai" : 31},{"juin" : 30},{"juillet" : 31},{"août" : 31},{"septembre" : 30},{"octobre" : 31},{"novembre" : 30},{"decembre" : 31}];
+if (annee % 4 === 0 && annee % 100 !== 0 && mois === "fevrier") {
+    console.log(`Il y a ${tableauMois[1].fevrier + 1} jours en ${mois}/${annee}.`);
+} else if (mois === "janvier") {
+    console.log(`Il y a ${tableauMois[0].janvier} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "fevrier") {
+    console.log(`Il y a ${tableauMois[1].fevrier} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "mars") {
+    console.log(`Il y a ${tableauMois[2].mars} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "avril") {
+    console.log(`Il y a ${tableauMois[3].avril} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "mai") {
+    console.log(`Il y a ${tableauMois[4].mai} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "juin") {
+    console.log(`Il y a ${tableauMois[5].juin} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "juillet") {
+    console.log(`Il y a ${tableauMois[6].juillet} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "août") {
+    console.log(`Il y a ${tableauMois[7].août} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "septembre") {
+    console.log(`Il y a ${tableauMois[8].septembre} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "octobre") {
+    console.log(`Il y a ${tableauMois[9].octobre} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "octobre") {
+    console.log(`Il y a ${tableauMois[10].octobre} jours en ${mois}/${annee}.`);
+}
+ else if (mois === "decembre") {
+    console.log(`Il y a ${tableauMois[11].decembre} jours en ${mois}/${annee}.`);
+} else {
+    console.log("Veuiller bien saisir le moi");
+}
 // Afficher le résultat
-console.log(`Il y a ${nombreDeJours} jours en ${mois}/${annee}.`);
